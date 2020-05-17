035d4c1d92990b6f86458b68ad273a2836434d6a
John Hennig <jhen@gmx.net>
2020-05-17 21:56:18 +0200

    Reworded documentation of doc-gen configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,19 @@
 ﻿"""
 Configuration file for rendering the documentation.
 
-The files in this folder are used to render the documentation of this
-package, from its source files, as a static web site. The renderer is
-the documentation generator Sphinx. It is configured by this very
-script and would be invoked on the command line via, on any operating
-system, via `sphinx-build . rendered`. The static HTML then ends up in
-the sub-folder `rendered`, where `index.html` is the start page.
+This folder contains the documentation source files that are to be
+rendered as a static web site by the documentation generator Sphinx.
+The rendering process is configured by this very script and would be
+triggered by running  `sphinx-build . rendered` on the command line,
+no matter the operating system. The rendered HTML then ends up in the
+sub-folder `rendered`, where `index.html` is the start page.
 
-The source files are the `.md` files here, where `index.md` maps to
-the start page, as well as the documentation string in the package's
-source code for the API documentation.
+The documentation source is comprised of the `.md` files here, of which
+`index.md` maps to the start page, as well as the documentation strings
+in the package's source code for the API documentation.
 
 All text may use mark-up according to the CommonMark specification of
-the Markdown syntax. The Sphinx extension `recommonmark` is used to
+the Markdown syntax. The Sphinx extension `reCommonMark` is used to
 convert Markdown to reStructuredText, Sphinx's native input format.
 """
 __license__ = 'MIT'
