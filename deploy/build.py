9c3f539fc1f4fcc6561003bb9d1016bb8fef8982
Max Elfner <maximilian.elfner@kit.edu>
2021-02-09 14:40:46 +0100

    Added shell to deploy script
    This will resolve issues on UNIX systems with the run() command calling a single string

--- deploy/build.py
+++ deploy/build.py
@@ -7,11 +7,11 @@ from shutil import move
 
 root = Path(__file__).absolute().parent.parent
 
-process = run('sphinx-build docs deploy/docs', cwd=root)
+process = run('sphinx-build docs deploy/docs', cwd=root, shell=True)
 if process.returncode:
     raise RuntimeError('Error while building documentation.')
 
-process = run('flit build --format wheel', cwd=root)
+process = run('flit build --format wheel', cwd=root, shell=True)
 if process.returncode:
     raise RuntimeError('Error while building wheel.')
 move(root/'dist', root/'deploy'/'dist')
