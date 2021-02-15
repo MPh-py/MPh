0b111c2e3c8966049e7e0c91572e2cfbf721cf02
John Hennig <jhen@gmx.net>
2021-02-15 16:17:03 +0100

    Check for existing target folder in build script.

--- deploy/build.py
+++ deploy/build.py
@@ -3,7 +3,7 @@ __license__ = 'MIT'
 
 from subprocess import run
 from pathlib import Path
-from shutil import move
+from shutil import rmtree
 
 root = Path(__file__).absolute().parent.parent
 
@@ -14,4 +14,8 @@ if process.returncode:
 process = run('flit build --format wheel', cwd=root, shell=True)
 if process.returncode:
     raise RuntimeError('Error while building wheel.')
-move(root/'dist', root/'deploy'/'dist')
+source = root/'dist'
+target = root/'deploy'/'dist'
+if target.exists():
+    rmtree(target)
+source.rename(target)
