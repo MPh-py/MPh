1231c0a629662733ffc7b23e87c0267cb14641e1
John Hennig <jhen@gmx.net>
2021-04-10 15:45:01 +0200

    Search data types recursively to cover entire model tree.

--- sandbox/list_datatypes.py
+++ sandbox/list_datatypes.py
@@ -3,48 +3,33 @@
 import mph
 
 
+def search_tree(node):
+    java = node.java
+    names = list(java.properties()) if hasattr(java, 'properties') else []
+    for name in names:
+        datatype = str(java.getValueType(name))
+        if datatype not in datatypes:
+            datatypes[datatype] = []
+        datatypes[datatype].append((node, name))
+    for child in node:
+        search_tree(child)
+
+
 def list_datatypes():
     for datatype in sorted(datatypes):
         print(f'• {datatype}')
 
 
 def list_properties(datatype):
-    for (group, node, property) in datatypes[datatype]:
-        print(f'  {group:10} -> {str(node.name()):20} -> {property}')
+    for (node, name) in datatypes[datatype]:
+        print(f'{node} → {name}')
 
 
 client = mph.start()
 model = client.load('../tests/capacitor.mph')
 
-groups = {
-    'functions':    model.java.func,
-    'components':   model.java.component,
-    'geometries':   model.java.geom,
-    'views':        model.java.view,
-    'selections':   model.java.selection,
-    'variables':    model.java.variable,
-    'physics':      model.java.physics,
-    'multiphysics': model.java.multiphysics,
-    'materials':    model.java.material,
-    'meshes':       model.java.mesh,
-    'studies':      model.java.study,
-    'solutions':    model.java.sol,
-    'plots':        model.java.result,
-    'datasets':     model.java.result().dataset,
-    'exports':      model.java.result().export,
-}
-
 datatypes = {}
-for (group, node) in groups.items():
-    nodes = [node(tag) for tag in node().tags()]
-    for node in nodes:
-        if not hasattr(node, 'properties'):
-            continue
-        for property in node.properties():
-            datatype = str(node.getValueType(property))
-            if datatype not in datatypes:
-                datatypes[datatype] = []
-            datatypes[datatype].append((group, node, str(property)))
+search_tree(model/None)
 
 print(f'Found {len(datatypes)} different data types.')
 list_datatypes()
