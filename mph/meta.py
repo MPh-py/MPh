9f4efa04abdd0cfdb8b119e2dc5a0fa43aabeec7
John Hennig <jhen@gmx.net>
2021-10-21 01:05:36 +0200

    Bumped version to 1.1.0.

--- mph/meta.py
+++ mph/meta.py
@@ -2,8 +2,8 @@
 
 title     = 'MPh'
 synopsis  = 'Pythonic scripting interface for Comsol Multiphysics'
-version   = '1.0.4'
-date      = '2021–05–23'
+version   = '1.1.0'
+date      = '2021–10–20'
 author    = 'John Hennig'
 copyright = '2020, John Hennig'
 license   = 'MIT'
