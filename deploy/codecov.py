cc0b7f71dc5d54b8ea9ad4936eb30b493ae51726
John Hennig <jhen@gmx.net>
2021-12-02 17:20:53 +0100

    Fixed uploader script for coverage reports.

--- deploy/codecov.py
+++ deploy/codecov.py
@@ -8,5 +8,5 @@ token = environ.get('MPh_CodeCov_token', None)
 if not token:
     raise RuntimeError('CodeCov upload token not set in environment.')
 
-root = Path(__file__).resolve().parent.parent.parent
+root = Path(__file__).resolve().parent.parent
 run(['codecov', '--file', 'coverage.xml', '--token', token], cwd=root)
