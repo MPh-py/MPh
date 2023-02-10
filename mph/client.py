6e3fdbe6fec0f310f7c7e19370fb6bf71a91f0b4
Andreas Maeder <68421682+maederan201@users.noreply.github.com>
2023-02-10 23:16:18 +0100

    Change classpath to `apiplugins` in client-server mode (#114)
    This fixes the "connect failure" in client-server mode with Comsol 6.1
    on certain Linux distributions, such as Ubuntu 22.04 LTS. According to
    Comsol Support, `apiplugins` as opposed to `plugins` is the correct
    folder to load the Java libraries from when not using a stand-alone
    client, see issue #114.

--- mph/client.py
+++ mph/client.py
@@ -168,6 +168,9 @@ class Client:
             jre  = backend['jvm'].parent.parent
             os.environ['PATH'] = str(jre) + os.pathsep + path
 
+        # This is a stand-alone client if no port given.
+        standalone = host and not port
+
         # Start the Java virtual machine.
         log.debug(f'JPype version is {jpype.__version__}.')
         log.info('Starting Java virtual machine.')
@@ -176,15 +179,15 @@ class Client:
         if option('classkit'):
             args += ['-Dcs.ckl']
         log.debug(f'JVM arguments: {args}')
-        jpype.startJVM(*args, classpath=str(root/'plugins'/'*'))
+        if standalone:
+            jpype.startJVM(*args, classpath=str(root/'plugins'/'*'))
+        else:
+            jpype.startJVM(*args, classpath=str(root/'apiplugins'/'*'))
         log.info('Java virtual machine has started.')
 
         # Import Comsol client object, a static class, i.e. singleton.
         # See `ModelUtil()` constructor in [1].
         from com.comsol.model.util import ModelUtil as java
-
-        # This is a stand-alone client if no port given.
-        standalone = host and not port
 
         # Possibly initialize the stand-alone client.
         if standalone:
