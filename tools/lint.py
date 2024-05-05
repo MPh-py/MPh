64758d0bc2fea3182ce31c84ddc08e30437af5db
John Hennig <jhen@gmx.net>
2024-05-06 00:10:12 +0200

    Added tooling script that invokes the code linter.
    This is just for consistency. So that all dev tools are obvious just
    from looking into the `tooling` folder. For now, this particular script
    is super simple, as it just calls Flake8. But we might add a type
    checker going forward, which would then be called as part of the linting
    step.

---
+++ tools/lint.py
@@ -0,0 +1,7 @@
+﻿"""Lints the code for style errors."""
+
+from subprocess import run
+from pathlib import Path
+
+root = Path(__file__).resolve().parent.parent
+run(['flake8'], cwd=root, check=True)
