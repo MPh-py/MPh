ea4c4788a49ad729e2e0ce1be74f44d65ff8e3bd
John Hennig <jhen@gmx.net>
2024-06-09 20:26:19 +0200

    Added dev-tool option to build docs locally from a clean slate.
    This is sometimes necessary so that Sphinx doesn't use its build cache.

--- tools/docs.py
+++ tools/docs.py
@@ -2,6 +2,19 @@
 
 from subprocess import run
 from pathlib import Path
+from sys import argv as arguments
+from shutil import rmtree
+
 
 root = Path(__file__).resolve().parent.parent
+source = root/'docs'
+target = root/'build'/'docs'
+
+# Build from a clean slate if command-line argument "clean" is passed.
+# For certain changes, such as to the custom style sheet, Sphinx would
+# otherwise use the cached files from the last build.
+if len(arguments) > 1 and arguments[1] == "clean":
+    if target.exists():
+        rmtree(target)
+
 run(['sphinx-build', 'docs', 'build/docs'], cwd=root, check=True)
