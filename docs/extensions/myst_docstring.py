98faaab8bc1a219f8057eda5d6ec7827b18200f2
John Hennig <jhen@gmx.net>
2025-05-04 16:56:07 +0200

    Fixed rendering of API documentation.
    This was due to a change in Sphinx 8.2 that broke our custom MyST
    extension to get Markdown support in doc-strings. It was not due to a
    change in MyST itself, as suspected in the last commit. MyST happens
    to restrict the Sphinx version, which is why our pin on MyST also pinned
    Sphinx.
    
    Essentially, Sphinx's Autodoc extension now has a new internal method
    `Documenter._generate()`, in addition to the existing `.generate()`.
    And in some cases it calls the internal method directly, bypassing
    the existing method, as what it does is redundant. This broke our hack
    as we then didn't get a chance to close the Sphinx domain directives we
    had opened.
    
    Also fixed some typos, polished doc-strings and code comments, added
    type hints to method signatures.

--- docs/extensions/myst_docstring.py
+++ docs/extensions/myst_docstring.py
@@ -1,70 +1,64 @@
 ﻿"""
 MyST-compatible drop-in replacement for Sphinx's Autodoc extension
 
-This extension overrides Autodoc's generation of [domain] directives
-so that the syntax is what the MyST Markdown parser expects, instead of
-Sphinx's own reStructuredText parser.
+This extension overrides Autodoc's generation of [domain] directives so that
+it outputs MyST-flavored Markdown instead of reStructuredText. We can then use
+Markdown in doc-strings and even leverage MyST's added features, such as
+cross-references to other parts of the documentation.
 
-Note that this is very much a hack, and that we simply assume that all
-doc-strings are written in MyST-flavored Markdown. However, unless your
-mileage differs, this here does make all of Sphinx's features available
-in Markdown, e.g. cross-references to other parts of the documentation.
+Note that this is very much a hack. Requires Sphinx >= 8.2.
 
-© 2022–2023 John Hennig, [MIT license]
+© 2022–2025 John Hennig, [MIT license]
 
 [domain]: https://www.sphinx-doc.org/en/master/usage/restructuredtext\
 /domains.html
 [MIT license]: https://www.tldrlegal.com/license/mit-license
 """
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 
 from sphinx.ext import autodoc
+from docutils.statemachine import StringList
 
 
 class Documenter(autodoc.Documenter):
     """
-    Mix-in to override content generation by `Documenter` class.
+    Mix-in to override content generation by `Documenter` class
 
-    All of Autodoc's documenter classes (for modules, functions, etc.)
-    derive from the `Documenter` base class. Three of its methods are
-    important:
+    All of Autodoc's documenter classes (for modules, functions, etc.) derive
+    from the `Documenter` base class. Three of its methods are important:
     * The one that adds the directive header, i.e. opens the directive.
-    * The one that generates the content, which includes the actual
-      doc-string as well as documentation for all child elements, such
-      as methods of a class.
+    * The one that generates the content, which includes the actual doc-string
+      as well as documentation for all child elements, such as methods of a
+      class.
     * The one methods that adds lines of generated mark-up.
 
-    We assume that all doc-strings are written in Markdown. All we
-    need to do then is convert the reStructuredText that Autodoc
-    generates to MyST-flavored Markdown. There isn't much syntax
-    complexity to worry about (or so we presume), so we'll just search
-    and replace the syntax of domain directives that Autodoc adds.
+    We assume that all doc-strings are written in Markdown. All we need to do
+    then is convert the reStructuredText that Autodoc generates to Markdown
+    that uses MyST's syntax extensions for reStructuredText directives.
 
     A complication, however, is that in reStructuredText the scope of
-    directives is designated by indentation, whereas in Markdown
-    directives are delimited by enclosing fences (triple back-ticks).
-    These would typically be nested by adding extra back-ticks at each
-    nesting level, for example four back-ticks for the outer directive
-    when a directive delimited with the regular three back-ticks is
-    nested inside.
+    directives is designated by indentation, whereas in Markdown directives are
+    delimited by enclosing fences (triple back-ticks). These would typically be
+    nested by adding extra back-ticks at each nesting level, for example four
+    back-ticks for the outer directive when a directive delimited with the
+    regular three back-ticks is nested inside.
 
-    To make our life easier, we use a little known feature of MyST, or
-    perhaps Markdown-it: We may also nest directives delimited by
-    exactly three back-ticks, ` ``` `, inside directives delimited by
-    three tildes, `~~~`. Then indentation *is* significant, and we
-    don't have to keep track of additional back-ticks. Though we
-    still need to close the directive's scope.
+    To make our life easier, we use a little known feature of MyST, or perhaps
+    Markdown-it: We may also nest directives delimited by exactly three
+    back-ticks, ` ``` `, inside directives delimited by three tildes, `~~~`.
+    Then indentation *is* significant, and we don't have to keep track of
+    additional back-ticks. Though we still need to close the directive's scope.
 
-    Ideally, Autodoc would be aware of such structural syntax
-    requirements. But it's not. And it doesn't call out to the parser
-    anyway, it simply generates reStructuredText no matter what. A
-    robust solution would either make Autodoc parser-aware or, better,
-    not have it generate intermediate mark-up at all and instead
-    produce Docutils nodes like Sphinx extensions should actually do.
+    Ideally, Autodoc would be aware of such structural syntax requirements. But
+    it's not. And it doesn't call out to the parser anyway, it simply generates
+    reStructuredText no matter what. A robust solution would either make
+    Autodoc parser-aware or, better, not have it generate intermediate mark-up
+    at all, but produce Docutils nodes directly, just like Sphinx extensions
+    usually do.
     """
 
-    def add_directive_header(self, sig):
+    def add_directive_header(self, sig: str) -> None:
         """Adds the directive header and options."""
 
         # Remembered we opened this directive.
@@ -87,11 +81,19 @@ class Documenter(autodoc.Documenter):
             self.directives.pop()
             self.add_line('~~~', self.get_sourcename())
 
-    def generate(self, **arguments):
+    def _generate(
+        self,
+        more_content: StringList = None,
+        real_modname: str        = None,
+        check_module: bool       = False,
+        all_members:  bool       = False,
+    ) -> None:
         """Generates documentation for the object and its members."""
 
         # Let super method render directive body.
-        super().generate(**arguments)
+        super()._generate(
+            more_content, real_modname, check_module, all_members,
+        )
 
         # Close the directive if it's the one we last opened.
         if not hasattr(self, 'directives') or not self.directives:
@@ -108,14 +110,14 @@ class Documenter(autodoc.Documenter):
         self.add_line(prefix, self.get_sourcename())
 
     def add_line(self, line: str, source: str, *lineno: int) -> None:
-        """Append one line of generated mark-up to the output."""
+        """Appends one line of generated mark-up to the output."""
 
         # Pass through empty lines.
         if not line.strip():
             self.directive.result.append('', source, *lineno)
             return
 
-        # Convert syntax of domain directives at start of line.
+        # Convert syntax of domain directives at beginning of line.
         domain = getattr(self, 'domain', 'py')
         directive = getattr(self, 'directivetype', self.objtype)
         prefix = f'.. {domain}:{directive}::'
@@ -172,9 +174,9 @@ def setup(app):
     """
     Sets up the extension.
 
-    Sphinx calls this function if the user named this extension in
-    `conf.py`. We then set up the Autodoc extension that ships with
-    Sphinx and override the generation of domain directives.
+    Sphinx calls this function if the user named this extension in `conf.py`.
+    We then set up the Autodoc extension that ships with Sphinx and override
+    the generation of domain directives.
     """
     app.setup_extension('sphinx.ext.autodoc')
     app.add_autodocumenter(ModuleDocumenter, override=True)
